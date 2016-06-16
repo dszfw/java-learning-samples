e90afdf67cadf1066a282573b974b87486dc0770
m.bobyleu <m.bobyleu@godeltech.com>
2016-06-16 14:58:08 +0300

    primitive .class literal

---
+++ src/exmpls/PrimitiveAndObjectGetClasses.java
@@ -0,0 +1,11 @@
+package exmpls;
+
+public class PrimitiveAndObjectGetClasses {
+    public static void main(String[] args) {
+        System.out.println(int.class);
+        System.out.println(Integer.class);
+        System.out.println(double.class);
+        System.out.println(int[].class);
+        System.out.println(String[].class);
+    }
+}
